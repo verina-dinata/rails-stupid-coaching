a537c66cdd8a6a2ab1723dbf36440f41bffb5103
Verina Ong Dinata <verina.od1997@gmail.com>
2022-07-23 13:39:41 +0800

    Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @input = params[:message]
+    if @input == 'I am going to work'
+      @reply = 'Great!'
+    elsif @input.end_with?('?')
+      @reply = 'Silly question, get dressed and go to work!'
+    else
+      @reply = "I don't care, get dressed and go to work!"
+    end
+  end
 end
